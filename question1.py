8b4b5fab2db123cc532b41fc38ae2620bb6b010c
ericwu970601 <165352753+ericwu970601@users.noreply.github.com>
2024-05-24 11:08:54 +0800

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,11 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+for i in range(5):  # 迴圈從 0 到 4，總共 5 次，每次迴圈內會打印兩行，總共 10 行
+    print("*+" * 10)  # 打印由 '*+' 重複 10 次生成的字串，即 '*+*+*+*+*+*+*+*+*+*+'
+    print("+*" * 10)  # 打印由 '+*' 重複 10 次生成的字串，即 '+*+*+*+*+*+*+*+*+*+*'
